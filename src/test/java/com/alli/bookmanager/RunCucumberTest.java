e47a12ad9e854f34a7fee5396f74cb51eda6e845
Allison Meunier <a.meunier@live.be>
2025-07-11 14:32:54 +0200

    BOOK-3: Added BDD with Cucumber and feature file

--- src/test/java/com/alli/bookmanager/RunCucumberTest.java
+++ src/test/java/com/alli/bookmanager/RunCucumberTest.java
@@ -9,7 +9,7 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features") // adjust if needed
+@SelectClasspathResource("com/alli/bookmanager/features") // adjust if needed
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 public class RunCucumberTest {
 }
